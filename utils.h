4e60b5e00754eaceae48618157783e2e84acff10
GeorgeLS <sdi1600094@di.uoa.gr>
2018-05-02 08:31:10 +0300

    fixed typo

--- utils.h
+++ utils.h
@@ -7,7 +7,7 @@
 
 #include "macros.h"
 
-/* Macros and functions for strechy buffers. Author: Sean Barett. */
+/* Macros and functions for strechy buffers. Author: Sean Barrett. */
 
 #define stb_sb_free(a)         ((a) ? free(stb__sbraw(a)),0 : 0)
 #define stb_sb_push(a,v)       (stb__sbmaybegrow(a,1), (a)[stb__sbn(a)++] = (v))
